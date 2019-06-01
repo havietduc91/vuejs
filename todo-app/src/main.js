fb62ffd9326344e62cb0b761704abf3945055560
VietED Deployment <vieted.deployment@gmail.com>
2019-06-01 07:16:11 +0700

    Fix bug codacy CI

--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  render: h => h(App),
+  el: "#app",
+  render: (h)=> h(App),
   router,
   store
 })
--- todo-app/src/store.js
+++ todo-app/src/store.js
@@ -1,78 +1,78 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     todos: [{
-      title: 'Item A',
-      project: 'Project A',
+      title: "Item A",
+      project: "Project A",
       done: false
     }, {
-      title: 'Item B',
-      project: 'Project B',
+      title: "Item B",
+      project: "Project B",
       done: true
     }, {
-      title: 'Item C',
-      project: 'Project C',
+      title: "Item C",
+      project: "Project C",
       done: false
     }, {
-      title: 'Item D',
-      project: 'Project D',
+      title: "Item D",
+      project: "Project D",
       done: false
     }],
-    newTodo: ''
+    newTodo: ""
   },
   mutations: {
     GET_TODO (state, todo) {
-      state.newTodo = todo
+      state.newTodo = todo;
     },
     ADD_TODO (state) {
       state.todos.push({
         body: state.newTodo,
-        completed: false
-      })
+        completed: false,
+      });
     },
     EDIT_TODO (state, todo) {
-      var todos = state.todos
-      todos.splice(todos.indexOf(todo), 1)
-      state.todos = todos
-      state.newTodo = todo.body
+      var todos = state.todos;
+      todos.splice(todos.indexOf(todo), 1);
+      state.todos = todos;
+      state.newTodo = todo.body;
     },
     REMOVE_TODO (state, todo) {
-      var todos = state.todos
-      todos.splice(todos.indexOf(todo), 1)
+      var todos = state.todos;
+      todos.splice(todos.indexOf(todo), 1);
     },
     COMPLETE_TODO (state, todo) {
-      todo.completed = !todo.completed
+      todo.completed = !todo.completed;
     },
     CLEAR_TODO (state) {
-      state.newTodo = ''
-    }
+      state.newTodo = "";
+    },
   },
   actions: {
     getTodo ({ commit }, todo) {
-      commit('GET_TODO', todo)
+      commit("GET_TODO", todo);
     },
     addTodo ({ commit }) {
-      commit('ADD_TODO')
+      commit("ADD_TODO");
     },
     editTodo ({ commit }, todo) {
-      commit('EDIT_TODO', todo)
+      commit("EDIT_TODO", todo);
     },
     removeTodo ({ commit }, todo) {
-      commit('REMOVE_TODO', todo)
+      commit("REMOVE_TODO", todo);
     },
     completeTodo ({ commit }, todo) {
-      commit('COMPLETE_TODO', todo)
+      commit("COMPLETE_TODO", todo);
     },
     clearTodo ({ commit }) {
-      commit('CLEAR_TODO')
-    }
+      commit("CLEAR_TODO");
+    },
   },
   getters: {
     getTodos (state) {
-      return state.todos
-    }
-  }
+      return state.todos;
+    },
+  },
 })
