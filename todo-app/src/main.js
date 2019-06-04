965a3efc9a10a0793de8e9ec94b6524ac505489f
havietduc91 <havietduc91@gmail.com>
2019-06-04 09:08:28 +0700

    Add alert to project base on vue boostrap

--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 new Vue({
   el: '#app',
