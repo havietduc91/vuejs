2e2cbd25bc3003ccab3c79a6df54754bd244dd96
havietduc91 <havietduc91@gmail.com>
2019-06-04 14:55:39 +0700

    Format code

--- todo-app/src/components/boostrap-samples/Table.vue
+++ todo-app/src/components/boostrap-samples/Table.vue
@@ -25,33 +25,33 @@
 
 <script>
   import InnerTable from './InnerTable';
-    export default {
-        components: {
-            InnerTable
-        },
-        data() {
-            return {
-                fields: [
-                    // A virtual column that doesn't exist in items
-                    'index',
-                    //Nested table
-                    'inner_table',
-                    // A column that needs custom formatting
-                    { key: 'name', label: 'Full Name' },
-                    // A regular column
-                    'age',
-                    // A regular column
-                    'sex',
-                    // A virtual column made up from two fields
-                    { key: 'nameage', label: 'First name and age' }
-                ],
-                items: [
-                    { name: { first: 'John', last: 'Doe' }, sex: 'Male', age: 42 },
-                    { name: { first: 'Jane', last: 'Doe' }, sex: 'Female', age: 36 },
-                    { name: { first: 'Rubin', last: 'Kincade' }, sex: 'Male', age: 73 },
-                    { name: { first: 'Shirley', last: 'Partridge' }, sex: 'Female', age: 62 }
-                ]
-            }
-        }
+  export default {
+    components: {
+      InnerTable
+    },
+    data() {
+      return {
+        fields: [
+          // A virtual column that doesn't exist in items
+          'index',
+          //Nested table
+          'inner_table',
+          // A column that needs custom formatting
+          { key: 'name', label: 'Full Name' },
+          // A regular column
+          'age',
+          // A regular column
+          'sex',
+          // A virtual column made up from two fields
+          { key: 'nameage', label: 'First name and age' }
+        ],
+        items: [
+          { name: { first: 'John', last: 'Doe' }, sex: 'Male', age: 42 },
+          { name: { first: 'Jane', last: 'Doe' }, sex: 'Female', age: 36 },
+          { name: { first: 'Rubin', last: 'Kincade' }, sex: 'Male', age: 73 },
+          { name: { first: 'Shirley', last: 'Partridge' }, sex: 'Female', age: 62 }
+        ]
+      }
     }
+  }
 </script>
