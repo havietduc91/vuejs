066bd6c1f61b04a01c72c5e1f7219b6a6e1485fe
havietduc91 <havietduc91@gmail.com>
2019-06-04 13:57:58 +0700

    Add boostrap table with sortable

--- todo-app/src/components/boostrap-samples/Table.vue
+++ todo-app/src/components/boostrap-samples/Table.vue
@@ -5,56 +5,56 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                // Note 'age' is left out and will not appear in the rendered table
-                fields: {
-                    last_name: {
-                        label: 'Person last name',
-                        sortable: true
-                    },
-                    first_name: {
-                        label: 'Person first name',
-                        sortable: false
-                    },
-                    city: {
-                        key: 'address.city',
-                        label: 'City',
-                        sortable: true
-                    },
-                    'address.country': {
-                        label: 'Country',
-                        sortable: true
-                    }
-                },
-                items: [
-                    {
-                        age: 40,
-                        first_name: 'Dickerson',
-                        last_name: 'Macdonald',
-                        address: { country: 'USA', city: 'New York' }
-                    },
-                    {
-                        age: 21,
-                        first_name: 'Larsen',
-                        last_name: 'Shaw',
-                        address: { country: 'Canada', city: 'Toronto' }
-                    },
-                    {
-                        age: 89,
-                        first_name: 'Geneva',
-                        last_name: 'Wilson',
-                        address: { country: 'Australia', city: 'Sydney' }
-                    },
-                    {
-                        age: 38,
-                        first_name: 'Jami',
-                        last_name: 'Carney',
-                        address: { country: 'England', city: 'London' }
-                    }
-                ]
-            }
-        }
+  export default {
+    data() {
+      return {
+        // Note 'age' is left out and will not appear in the rendered table
+        fields: {
+          last_name: {
+            label: 'Person last name',
+            sortable: true
+          },
+          first_name: {
+            label: 'Person first name',
+            sortable: false
+          },
+          city: {
+            key: 'address.city',
+            label: 'City',
+            sortable: true
+          },
+          'address.country': {
+            label: 'Country',
+            sortable: true
+          }
+        },
+        items: [
+          {
+            age: 40,
+            first_name: 'Dickerson',
+            last_name: 'Macdonald',
+            address: { country: 'USA', city: 'New York' }
+          },
+          {
+            age: 21,
+            first_name: 'Larsen',
+            last_name: 'Shaw',
+            address: { country: 'Canada', city: 'Toronto' }
+          },
+          {
+            age: 89,
+            first_name: 'Geneva',
+            last_name: 'Wilson',
+            address: { country: 'Australia', city: 'Sydney' }
+          },
+          {
+            age: 38,
+            first_name: 'Jami',
+            last_name: 'Carney',
+            address: { country: 'England', city: 'London' }
+          }
+        ]
+      }
     }
+  }
 </script>
