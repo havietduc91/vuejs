a28549a5d190109286b7f603cfc0ca92c7b0b8a7
havietduc91 <havietduc91@gmail.com>
2019-06-04 18:15:03 +0700

    Sortable, filterable and checkable for ag-grid sample

--- todo-app/src/components/grid/GridSample.vue
+++ todo-app/src/components/grid/GridSample.vue
@@ -6,6 +6,7 @@
           <ag-grid-vue style="width: 600px; height: 500px;"
                        class="ag-theme-balham"
                        :columnDefs="columnDefs"
+                       rowSelection="multiple"
                        :rowData="rowData">
           </ag-grid-vue>
         </b-col>
@@ -31,16 +32,13 @@
     },
     beforeMount() {
       this.columnDefs = [
-        {headerName: 'Make', field: 'make'},
-        {headerName: 'Model', field: 'model'},
-        {headerName: 'Price', field: 'price'}
+        {headerName: 'Make', field: 'make', sortable: true, filter: true, checkboxSelection: true},
+        {headerName: 'Model', field: 'model', sortable: true, filter: true },
+        {headerName: 'Price', field: 'price', sortable: true, filter: true }
       ];
-
-      this.rowData = [
-        {make: 'Toyota', model: 'Celica', price: 35000},
-        {make: 'Ford', model: 'Mondeo', price: 32000},
-        {make: 'Porsche', model: 'Boxter', price: 72000}
-      ];
+      fetch('https://api.myjson.com/bins/15psn9')
+          .then(result => result.json())
+          .then(rowData => this.rowData = rowData);
     }
   }
 </script>
