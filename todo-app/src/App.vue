aa01108142b7ae3df4a289e5ec909c604a8a25e4
linhdt <>
2019-06-05 17:50:03 +0700

    init total price component

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
     <app-navigation v-sticky></app-navigation>
-    <todo-list v-bind:todos="todos"></todo-list>
-    <create-todo v-on:add-todo="addTodo"></create-todo>
-    <div class="hello" ref="chartdiv"></div>
-    <pie-chart></pie-chart>
-    <x-y-chart></x-y-chart>
-    <alert show>Default Alert</alert>
-    <form-sample/>
-    <grid-sample/>
-    <table-sample style="margin-top: 20px;"/>
+    <!--<todo-list v-bind:todos="todos"></todo-list>-->
+    <!--<create-todo v-on:add-todo="addTodo"></create-todo>-->
+    <!--<div class="hello" ref="chartdiv"></div>-->
+    <!--<pie-chart></pie-chart>-->
+    <!--<x-y-chart></x-y-chart>-->
+    <!--<alert show>Default Alert</alert>-->
+    <!--<form-sample/>-->
+    <!--<grid-sample/>-->
+    <!--<table-sample style="margin-top: 20px;"/>-->
+
+    <total-price-component style="padding: 50px"></total-price-component>
     <app-navigation v-sticky:bottom></app-navigation>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -26,27 +28,28 @@
 </style>
 <script>
 //Commpn component
-import AppNavigation from './components/common/AppNavigation'
+import AppNavigation from "./components/common/AppNavigation";
 
 //Todo list samples
-import TodoList from './components/TodoList'
-import CreateTodo from './components/CreateTodo'
-import { mapGetters } from 'vuex'
+import TodoList from "./components/TodoList";
+import CreateTodo from "./components/CreateTodo";
+import { mapGetters } from "vuex";
 
 //Am charts samples
-import PieChart from './components/charts/PieChart'
-import XYChart from './components/charts/XYChart'
+import PieChart from "./components/charts/PieChart";
+import XYChart from "./components/charts/XYChart";
 
 //Vue boostrap samples
-import Alert from './components/boostrap-samples/Alert'
-import FormSample from './components/FormSample'
-import TableSample from './components/boostrap-samples/Table'
+import Alert from "./components/boostrap-samples/Alert";
+import FormSample from "./components/FormSample";
+import TableSample from "./components/boostrap-samples/Table";
 
 //Ag grid samples
-import GridSample from './components/grid/GridSample'
+import GridSample from "./components/grid/GridSample";
+import TotalPriceComponent from "./components/TotalPriceComponent";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     AppNavigation,
     TodoList,
@@ -57,42 +60,43 @@ export default {
     FormSample,
     TableSample,
     GridSample,
+    TotalPriceComponent
   },
   methods: {
-    addTodo (todo) {
-      this.todos.push(todo)
+    addTodo(todo) {
+      this.todos.push(todo);
     }
   },
   computed: {
     ...mapGetters({
-      todos: 'getTodos'
+      todos: "getTodos"
     })
-  },
-}
+  }
+};
 </script>
 <style>
-  body {
-    margin: 0;
-  }
-  #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-bottom: 30px;
-  }
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-    line-height: 60px;
-  }
+body {
+  margin: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-bottom: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  line-height: 60px;
+}
 
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  }
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
 <style lang="scss">
-  @import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
-  @import "../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css";
+@import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+@import "../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css";
 </style>
---
+++ todo-app/src/components/TotalPriceComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <b-col>
+    <b-row >
+      <b-col md="3" style="box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2)">
+        <div style="background: #ffffff;">
+          <b-row align-h="start">
+            <b-col md="6" align-self="start" style="text-align: left ">
+              <div>
+                <p>Invoices Summary</p>
+                <p>Month to Date</p>
+                <p>Value month to date</p>
+              </div>
+            </b-col>
+          </b-row>
+          <b-row align-h="end">
+            <b-col md="6" style="text-align: right ">
+              <div>
+                <p>12.5% $7,129.91</p>
+                <p>W8,1235,869.60</p>
+              </div>
+            </b-col>
+          </b-row>
+        </div>
+        <div style="background: #f5f5f5">
+          <b-row align-h="between">
+            <b-col style="text-align: left ">Cloud Usage Amount</b-col>
+            <b-col style="text-align: right ">$7.129.91</b-col> </b-row
+          ><b-row align-h="between">
+            <b-col style="text-align: left ">Cloud Usage Amount</b-col>
+            <b-col style="text-align: right ">$7.129.91</b-col> </b-row
+          ><b-row align-h="between">
+            <b-col style="text-align: left ">Cloud Usage Amount</b-col>
+            <b-col style="text-align: right ">$7.129.91</b-col> </b-row
+          ><b-row align-h="between">
+            <b-col style="text-align: left ">Cloud Usage Amount</b-col>
+            <b-col style="text-align: right ">$7.129.91</b-col>
+          </b-row>
+          <b-row align-h="between">
+            <b-col style="text-align: left ">Total discount</b-col>
+            <b-col style="text-align: right ">-$31,412.42</b-col>
+          </b-row>
+        </div>
+      </b-col>
+      <b-col md="9"></b-col>
+    </b-row>
+  </b-col>
+</template>
+
+<script>
+export default {
+  name: "TotalPriceComponent"
+};
+</script>
+
+<style scoped></style>
