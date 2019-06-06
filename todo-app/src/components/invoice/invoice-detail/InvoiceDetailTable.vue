711a95d9e5d1f7568592991b5c51fb77d5dcd78b
havietduc91 <havietduc91@gmail.com>
2019-06-06 10:15:14 +0700

    Use 2 spaces instead of 4 spaces

--- todo-app/src/components/invoice/invoice-detail/InvoiceDetailTable.vue
+++ todo-app/src/components/invoice/invoice-detail/InvoiceDetailTable.vue
@@ -21,116 +21,116 @@
   </div>
 </template>
 <script>
-    import { AgGridVue } from "ag-grid-vue";
-    import BCol from "bootstrap-vue/esm/components/layout/col";
+import { AgGridVue } from "ag-grid-vue";
+import BCol from "bootstrap-vue/esm/components/layout/col";
 
-    // import { LicenseManager } from "ag-grid-enterprise";
-    // LicenseManager.setLicenseKey("[TRIAL]_5_August_2019_[v2]_MTU2NDk2MzIwMDAwMA==cf4a41f302a394c24b79f5eddf332451");
+// import { LicenseManager } from "ag-grid-enterprise";
+// LicenseManager.setLicenseKey("[TRIAL]_5_August_2019_[v2]_MTU2NDk2MzIwMDAwMA==cf4a41f302a394c24b79f5eddf332451");
 
 
-    export default {
-        name: 'App',
-        components: {
-            BCol,
-            AgGridVue
-        },
-        data: function() {
-            return {
-                gridOptions: null,
-                gridApi: null,
-                columnApi: null,
-                columnDefs: null,
-                rowData: null,
-                groupDefaultExpanded: null,
-                getDataPath: null,
-                autoGroupColumnDef: null
-            };
-        },
-        beforeMount() {
-            this.gridOptions = {};
-            this.columnDefs = [{ field: "jobTitle" }, { field: "employmentType" }];
-            this.rowData = [
-                {
-                    orgHierarchy: ["Erica Rogers"],
-                    jobTitle: "CEO",
-                    employmentType: "Permanent"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett"],
-                    jobTitle: "Exec. Vice President",
-                    employmentType: "Permanent"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker"],
-                    jobTitle: "Director of Operations",
-                    employmentType: "Permanent"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker", "Brittany Hanson"],
-                    jobTitle: "Fleet Coordinator",
-                    employmentType: "Permanent"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker", "Brittany Hanson", "Leah Flowers"],
-                    jobTitle: "Parts Technician",
-                    employmentType: "Contract"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker", "Brittany Hanson", "Tammy Sutton"],
-                    jobTitle: "Service Technician",
-                    employmentType: "Contract"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker", "Derek Paul"],
-                    jobTitle: "Inventory Control",
-                    employmentType: "Permanent"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland"],
-                    jobTitle: "VP Sales",
-                    employmentType: "Permanent"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland", "Morris Hanson"],
-                    jobTitle: "Sales Manager",
-                    employmentType: "Permanent"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland", "Todd Tyler"],
-                    jobTitle: "Sales Executive",
-                    employmentType: "Contract"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland", "Bennie Wise"],
-                    jobTitle: "Sales Executive",
-                    employmentType: "Contract"
-                },
-                {
-                    orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland", "Joel Cooper"],
-                    jobTitle: "Sales Executive",
-                    employmentType: "Permanent"
-                }
-            ];
-            this.groupDefaultExpanded = -1;
-            this.getDataPath = data => {
-                return data.orgHierarchy;
-            };
-            this.autoGroupColumnDef = {
-                headerName: "Organisation Hierarchy",
-                cellRendererParams: { suppressCount: true }
-            };
-        },
-        mounted() {
-            this.gridApi = this.gridOptions.api;
-            this.gridColumnApi = this.gridOptions.columnApi;
-        },
-        methods: {
-            onFilterTextBoxChanged() {
-                this.gridApi.setQuickFilter(document.getElementById("filter-text-box").value);
-            },
-            onGridReady(params) {
-                params.api.sizeColumnsToFit();
-            }
-        }
+export default {
+  name: 'InvoiceDetaulTable',
+  components: {
+    BCol,
+    AgGridVue
+  },
+  data: function() {
+    return {
+      gridOptions: null,
+      gridApi: null,
+      columnApi: null,
+      columnDefs: null,
+      rowData: null,
+      groupDefaultExpanded: null,
+      getDataPath: null,
+      autoGroupColumnDef: null
+    };
+  },
+  beforeMount() {
+    this.gridOptions = {};
+    this.columnDefs = [{ field: "jobTitle" }, { field: "employmentType" }];
+    this.rowData = [
+      {
+        orgHierarchy: ["Erica Rogers"],
+        jobTitle: "CEO",
+        employmentType: "Permanent"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett"],
+        jobTitle: "Exec. Vice President",
+        employmentType: "Permanent"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker"],
+        jobTitle: "Director of Operations",
+        employmentType: "Permanent"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker", "Brittany Hanson"],
+        jobTitle: "Fleet Coordinator",
+        employmentType: "Permanent"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker", "Brittany Hanson", "Leah Flowers"],
+        jobTitle: "Parts Technician",
+        employmentType: "Contract"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker", "Brittany Hanson", "Tammy Sutton"],
+        jobTitle: "Service Technician",
+        employmentType: "Contract"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Esther Baker", "Derek Paul"],
+        jobTitle: "Inventory Control",
+        employmentType: "Permanent"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland"],
+        jobTitle: "VP Sales",
+        employmentType: "Permanent"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland", "Morris Hanson"],
+        jobTitle: "Sales Manager",
+        employmentType: "Permanent"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland", "Todd Tyler"],
+        jobTitle: "Sales Executive",
+        employmentType: "Contract"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland", "Bennie Wise"],
+        jobTitle: "Sales Executive",
+        employmentType: "Contract"
+      },
+      {
+        orgHierarchy: ["Erica Rogers", "Malcolm Barrett", "Francis Strickland", "Joel Cooper"],
+        jobTitle: "Sales Executive",
+        employmentType: "Permanent"
+      }
+    ];
+    this.groupDefaultExpanded = -1;
+    this.getDataPath = data => {
+      return data.orgHierarchy;
+    };
+    this.autoGroupColumnDef = {
+      headerName: "Organisation Hierarchy",
+      cellRendererParams: { suppressCount: true }
+    };
+  },
+  mounted() {
+    this.gridApi = this.gridOptions.api;
+    this.gridColumnApi = this.gridOptions.columnApi;
+  },
+  methods: {
+    onFilterTextBoxChanged() {
+      this.gridApi.setQuickFilter(document.getElementById("filter-text-box").value);
+    },
+    onGridReady(params) {
+      params.api.sizeColumnsToFit();
     }
+  }
+}
 </script>
