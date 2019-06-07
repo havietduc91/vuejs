5d6583c7a3cf40e85a23de9a57c4b534994348b1
havietduc91 <havietduc91@gmail.com>
2019-06-07 14:53:08 +0700

    Fix eslint errors

--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -3,41 +3,40 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
-
-//Vue use bootstrap
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue)
-
-
-//Vue use directives
-// Demo #1
-Vue.directive("sticky", function(el, binding, vnode) {
-    el.style.position = "fixed";
-});
-
-// Demo #4 - Using argument to define how the directive behaves
-// I want application to have a header and a footer. I can use
-// one custom directive to define the behaviors of both
-Vue.directive("sticky", function(el, binding, vnode) {
-    const loc = binding.arg === "bottom" ? "bottom" : "top";
-    el.style.position = "fixed";
-    el.style[loc] = 0;
-    if (loc === "bottom") {
-        el.style.background = "burlywood";
-    } else {
-        el.style.background = "#7e7e7e";
-    }
-});
-
-//Use font-awesome for v
+// Use font-awesome for v
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret, faSearch, faSortDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faUserSecret, faSearch, faSortDown)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+// Vue use bootstrap
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+// Vue use directives
+// Demo #1
+Vue.directive('sticky', function (el, binding, vnode) {
+  el.style.position = 'fixed'
+})
+
+// Demo #4 - Using argument to define how the directive behaves
+// I want application to have a header and a footer. I can use
+// one custom directive to define the behaviors of both
+Vue.directive('sticky', function (el, binding, vnode) {
+  const loc = binding.arg === 'bottom' ? 'bottom' : 'top'
+  el.style.position = 'fixed'
+  el.style[loc] = 0
+  if (loc === 'bottom') {
+    el.style.background = 'burlywood'
+  } else {
+    el.style.background = '#7e7e7e'
+  }
+})
 
 new Vue({
   el: '#app',
