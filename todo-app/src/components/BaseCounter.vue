18bf494366c60598a039ca4ada9173444a6c6ce3
havietduc91 <havietduc91@gmail.com>
2019-06-10 17:07:11 +0700

    Add Base counter and karma to support testing for frontend side

---
+++ todo-app/src/components/BaseCounter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ count }}
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+// This component to support test karma purpose
+export default {
+  name: 'BaseCounter',
+  data () {
+    return {
+      count: 0
+    }
+  },
+
+  methods: {
+    increment () {
+      this.count++
+    }
+  }
+}
+</script>
